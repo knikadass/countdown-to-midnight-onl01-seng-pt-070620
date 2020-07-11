cef3bbe4fd01c7d30746cc36c6cd672fd503dc0b
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 00:25:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,18 @@
 
 def countdown (seconds)
   while seconds > 0 
-  puts " #{seconds} SECOND(S)!"
+  puts "#{seconds} SECOND(S)!"
   seconds -= 1 
  end
  HAPY NEW YEAR!
+end
+
+
+def countdown_with_sleep (seconds)
+  while seconds > 0 
+  puts "#{seconds} SECOND(S)! "
+  sleep (1)
+  seconds -= 1 
+ end
+ HAPPY NEW YEAR!
 end
